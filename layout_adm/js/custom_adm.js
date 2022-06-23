4415a135215afbbed7680bb41327f4834e53f7b2
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2022-06-23 13:54:09 -0300

    main

---
+++ layout_adm/js/custom_adm.js
@@ -0,0 +1,10 @@
+let avatar = document.querySelector(".avatar"),
+  notification = document.querySelector(".notification");
+
+
+
+avatar.addEventListener("click", () => {
+    console.log('foi')
+  let dropdown_menu = document.querySelector(".dropdown-menu");
+  dropdown_menu.style.background = "red";
+});
