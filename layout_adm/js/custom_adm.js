499b85fd059b531a5ff34ab1a915b169d57354dd
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2022-06-25 11:11:37 -0300

    maiin

--- layout_adm/js/custom_adm.js
+++ layout_adm/js/custom_adm.js
@@ -1,10 +1,25 @@
+// dropdown navbar
+
 let avatar = document.querySelector(".avatar"),
   notification = document.querySelector(".notification");
 
+dropMenu(avatar)
+dropMenu(notification)
+
+function dropMenu(selector){
+  selector.addEventListener("click", () => {
+    let dropdownMenu = selector.querySelector(".dropdown-menu");
+    dropdownMenu.classList.contains("active")? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active")
+  })
+}
 
 
-avatar.addEventListener("click", () => {
-    console.log('foi')
-  let dropdown_menu = document.querySelector(".dropdown-menu");
-  dropdown_menu.style.background = "red";
-});
+
+
+
+// avatar.addEventListener("click", () => {
+//     console.log('foi')
+//   let dropdown_menu = document.querySelector(".dropdown-menu");
+//   dropdown_menu.style.background = "red";
+// });
+
