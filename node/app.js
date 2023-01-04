1ec28f3abff27dbdbbe64fb63a76aac1cfdaf70c
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-01-04 14:13:19 -0300

    celke

--- node/app.js
+++ node/app.js
@@ -5,4 +5,16 @@ app.get('/', (req, res) => {
     res.send("Salve mundo")
 })
 
+app.get('/contatos/:id', (req, res) => {
+    // res.send("visualizar contatos")
+    const {id} = req.params
+    const {sit} = req.query
+    return res.json({
+        sit: sit,
+        id: id,
+        "nome": "André",
+        "email": "andremartins746@gmai.com"
+    })
+})
+
 app.listen(3000, () => console.log('rodando '))
