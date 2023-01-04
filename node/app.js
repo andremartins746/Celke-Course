3ff16f3813d95b6c7d5a17fa8e18dc23e6f02e56
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-01-04 13:58:16 -0300

    celke Node Basicos

--- node/app.js
+++ node/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Olá mundo")
+    res.send("Salve mundo")
 })
 
 app.listen(3000, () => console.log('rodando '))
