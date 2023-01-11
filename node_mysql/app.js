3d669cc9eee349455267cf9f093cbdd9aed52fc4
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-01-11 14:16:53 -0300

    Node_mysql

--- node_mysql/app.js
+++ node_mysql/app.js
@@ -1,24 +1,12 @@
 const express = require("express")
 const app = express()
-app.use(express.urlencoded({extended: true}))
 
-app.get("/", (req, res) => {
-    res.json({
-        erro:false,
-        nome: "andré",
-        email: "andremartins746@gmail.com"
-    })
-})  
-
-app.get("/usuarios/:id", (req, res) => {
-    const {id} = req.params
-    res.json({
-        erro:false,
-        id: id,
-        nome: "andré",
-        email: "andremartins746@gmail.com"
-    })
-
+app.get('/', (req, res) => {
+res.json({
+    erro: false,
+    nome: "André",
+    email: "Andremartfins746@gmail.com"
+})
 })
 
-app.listen(3000, () => console.log('Servidor rodando em: localhost:3000/'))
+app.listen(3000, () => console.log("rodando"))
