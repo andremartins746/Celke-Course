f2b9e0315f05fcd2c9736d712e229e15ed860fb5
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-01-17 14:13:27 -0300

    commit

--- node_mysql/models/db.js
+++ node_mysql/models/db.js
@@ -4,6 +4,7 @@ const sequelize = new Sequelize('celke', 'root','', {
     host: 'localhost',
     dialect: 'mysql'
 }) 
+
 //TESTANDO A CONEXÃO PARA VER SE ESTA CONECTANDO COM O BD
 sequelize.authenticate()
     .then(() => {
