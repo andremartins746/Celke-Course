86b4af09129935e17882338aad1080e6f1fca45b
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-02-05 16:17:17 -0300

    aula5 React

--- react/reacao/src/App.js
+++ react/reacao/src/App.js
@@ -4,13 +4,22 @@ function App(props) {
 
   const Name = "André Martins Pereira"
 
+  const Usuario = {
+    idade: 20,
+    profissao: "Desenvolvedor de Software"
+  }
+
   const FromatarNome = (Name) => {
     return Name;
   };
 
+  const formatarDadosUsuario = (usuario) => {
+    return " tem, "+ usuario.idade + " anos, profissão " + usuario.profissao
+  }
+
   return (
     <div>
-      <h1>Olà. {FromatarNome(Name)}</h1>
+      <h1>Colaborador {FromatarNome(Name)} {formatarDadosUsuario(Usuario)}</h1>
     </div>
   );
 }
