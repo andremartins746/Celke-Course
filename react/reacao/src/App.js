7b67b027857ecadfe3546b7386e6a986623349fc
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-03-05 16:10:29 -0300

    useEffect

--- react/reacao/src/App.js
+++ react/reacao/src/App.js
@@ -1,35 +1,41 @@
 import "./App.css";
-import React from "react"
-import Header from "./components/Header/index"
-import Article from "./components/articles/index"
-import Menu from "./components/Menu/index"
-import Aside from "./components/Aside/index"
-import Footer from "./components/Footer";
-import Comment from "./components/Comment"
+import React, {useState} from "react"
 
-function App(props) {
+const App = () => {
 
-  const comentario = {
-    date: new Date(),
-    text: "asjdfhaskjlfbsbfisdf sdifhsd ifsidhuf sidfhsduhf sidfuhsudfhsdifhus dfihsudf sdfusdh",
-    author:{
-      name: "André Martins Pereira",
-      avatarURL: "https://placekitten.com/g/64/64"
-    }
-  }
 
-  return (
-    <div>
-      <h1>site </h1>
-      <Header/>
-      <Article></Article>
-      <Menu></Menu>
-      <Aside></Aside>
-      <Comment comentario = {comentario}></Comment>
-      <Footer></Footer>
-    </div>  
-  );
+  const [nome, SetNome] = useState('')
+  return(
+    <>
+        <p>{nome}</p>
+        <button onClick={() => SetNome("andre")}>Alterar</button>
+    </>
+  )
 }
+
+// function App(props) {
+// // 
+//   const comentario = {
+//     date: new Date(),
+//     text: "asjdfhaskjlfbsbfisdf sdifhsd ifsidhuf sidfhsduhf sidfuhsudfhsdifhus dfihsudf sdfusdh",
+//     author:{
+//       name: "André Martins Pereira",
+//       avatarURL: "https://placekitten.com/g/64/64"
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <h1>site </h1>
+//       <Header/>
+//       <Article></Article>
+//       <Menu></Menu>
+//       <Aside></Aside>
+//       <Comment comentario = {comentario}></Comment>
+//       <Footer></Footer>
+//     </div>  
+  // );
+// }
 
 // function Welcome(props){
 //   return (
