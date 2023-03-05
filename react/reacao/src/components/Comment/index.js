8d2e518719e0b24d364173326e63304fc4cde4b8
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2023-03-05 15:34:43 -0300

    props conceito

--- react/reacao/src/components/Comment/index.js
+++ react/reacao/src/components/Comment/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
+
+function Avatar (props) {
+    return (
+        <>
+            <img src={props.user} alt="autor"/> <br/> <hr></hr>
+        </>
+    )
+}
+
+
 const Comment = (props) => {
 
     function FormaData (date) {
@@ -7,7 +17,7 @@ const Comment = (props) => {
     }
     return (
         <>
-            <img src={props.comentario.author.avatarURL} alt="autor"/> <br/> <hr></hr>
+            <Avatar user = {props.comentario.author.avatarURL}></Avatar>
             <span>{props.comentario.author.name}</span> <br/><br/>
             <span>{props.comentario.text}</span> <br/><br/>
             <span>{FormaData(props.comentario.date)}</span> <br/><br/>
